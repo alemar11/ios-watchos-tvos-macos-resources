bd0dfa9f7cce8f85ddf170236cb404dba68f28d6
Alessandro Marzoli <alessandromarzoli@gmail.com>
2020-07-31 15:04:06 +0200

    py script to validate URLs

---
+++ validation.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import re
+import requests
+
+print("Checking URL integrity...")
+readme = open("README.md", "r")
+lines = readme.readlines() 
+
+for line in lines: 
+    # Anything that isn't a square closing bracket
+    name_regex = "[^]]+"
+    # http:// or https:// followed by anything but a closing paren
+    url_regex = "http[s]?://[^)]+"
+    markup_regex = '\[({0})]\(\s*({1})\s*\)'.format(name_regex, url_regex)
+
+    for match in re.findall(markup_regex, line):
+        length = len(match)
+        if length == 2:
+            source_name = match[0]
+            source_url = match[1]
+            # try catch, no retries
+            try:
+                r = requests.get(source_url)
+                r.raise_for_status()
+                print ("✅", source_name)
+            except:
+                print ("❌", source_name)
+
+        elif length == 1:
+            source_name = match[0]
+            print ("❌", source_name)
+        else:
+            print ("❌", line)
+print("Done.")            
